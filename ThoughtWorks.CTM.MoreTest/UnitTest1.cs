98f0e7a7e835c76558546b9094c57d26b83f6f44
Nikhil Rawat <nik.rawat123@gmail.com>
2020-08-16 23:08:26 +0530

    Added one more test project

---
+++ ThoughtWorks.CTM.MoreTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace ThoughtWorks.CTM.MoreTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(true);
+
+        }
+    }
+}
