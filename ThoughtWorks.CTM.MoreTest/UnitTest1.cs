fe6f399ad7a2fcb6e25d3155bd0600a557416588
Nikhil Rawat <nik.rawat123@gmail.com>
2020-08-16 23:48:09 +0530

    changes

--- ThoughtWorks.CTM.MoreTest/UnitTest1.cs
+++ ThoughtWorks.CTM.MoreTest/UnitTest1.cs
@@ -6,17 +6,6 @@ namespace ThoughtWorks.CTM.MoreTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-            Assert.True(true);
-
-        }
 
         [Fact]
         public void Process_WithNegativeTracks_success()
@@ -44,5 +33,22 @@ namespace ThoughtWorks.CTM.MoreTest
             Assert.Throws<Exception>(() => processor.ProcessFile(lines, 0));
 
         }
+
+        [Fact]
+        public void Process_With5Tracks_success()
+        {
+            //Arrange
+            var lines = File.ReadAllLines(@".\SampleInput.txt");
+            var processor = new Processor();
+
+            //Act
+            var conferenceTracks = processor.ProcessFile(lines, 5);
+            var output = processor.CreateProgram(conferenceTracks);
+
+            //Assert
+            Assert.NotEmpty(output);
+            Assert.Equal(5, conferenceTracks.Count);
+        }
+
     }
 }
--- ThoughtWorks.CTM.Test/ConferenceTrackManagementTest.cs
+++ ThoughtWorks.CTM.Test/ConferenceTrackManagementTest.cs
@@ -22,24 +22,9 @@ namespace ThoughtWorks.CTM.Test
             Assert.Equal(2, conferenceTracks.Count);
         }
 
-        [Fact]
-        public void Process_With5Tracks_success()
-        {
-            //Arrange
-            var lines = File.ReadAllLines(@".\SampleInput.txt");
-            var processor = new Processor();
+      
 
-            //Act
-            var conferenceTracks = processor.ProcessFile(lines, 5);
-            var output = processor.CreateProgram(conferenceTracks);
 
-            //Assert
-            Assert.NotEmpty(output);
-            Assert.Equal(5, conferenceTracks.Count);
-        }
 
-       
-
-       
     }
 }
